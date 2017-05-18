0b8a58a4295172928df70731d6d3bb92004fa1ba
Adam Cameron <adamdcameron@gmail.com>
2017-05-18 14:06:50 -0300

    commit at end of tutorial

--- diary.py
+++ diary.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
+import sys
 from collections import OrderedDict
 import datetime
 from peewee import *
+import os
 
 db = SqliteDatabase('diary.db')
 
@@ -34,21 +36,59 @@ def menu_loop():
         if choice in menu:
             menu[choice]()
 
+def clear():
+    os.system('cls' if os.name == 'nt' else 'clear')
+
 def add_entry():
-    """Add an entry"""
-    pass
+    """Add an entry."""
+    print("Enter your entry. Press Ctrl+D when finished.")
+    data = sys.stdin.read().strip()
 
-def view_entries():
+    if data:
+        if input("Save entry? [Y/n] ").lower() != 'n':
+            Entry.create(content=data)
+            print("Saved successfully!")
+
+def search_entries():
+    """Search entries for a string."""
+    view_entries(input("Search query: "))
+
+def delete_entry(entry):
+    """Delete an entry."""
+    if input("Are you sure? [yN] ").lower() == 'y':
+        entry.delete_instance()
+
+def view_entries(search_query=None):
     """View previous entries"""
-    pass
+    entries = Entry.select().order_by(Entry.timestamp.desc())
 
+    if search_query:
+        entries = entries.where(Entry.content.contains(search_query))
+
+    for entry in entries:
+        timestamp = entry.timestamp.strftime('%A %B %d, %Y %I:%M%p')
+        print(timestamp)
+        print('='*len(timestamp)) # Shortcut to underline something!
+        print(entry.content)
+        print('\n\n'+'='*len(timestamp))
+        print('n) next entry')
+        print('d) delete entry')
+        print('q) return to main menu')
+
+        next_action = input("Action: [Ndq] ").lower().strip()
+        if next_action == 'q':
+            break
+
+        elif next_action == 'd':
+            delete_entry(entry)
 def delete_entry(entry):
     """Delete an entry"""
     pass
 
 menu = OrderedDict([
     ('a', add_entry),
-    ('v', view_entries)
+    ('v', view_entries),
+    ('s', search_entries)
 ])
 
 if __name__ == '__main__':
